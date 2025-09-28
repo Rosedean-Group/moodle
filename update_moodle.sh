be4408dbe1682fcda395046008dfbdddbb7a3b38
Dan Champion <dan@rohallion.agency>
2025-09-28 20:23:00 +0100

    Update update_moodle.sh

--- update_moodle.sh
+++ update_moodle.sh
@@ -1,5 +1,5 @@
    #!/bin/bash
    git fetch upstream
-   for BRANCH in MOODLE_401_STABLE master; do
+   for BRANCH in MOODLE_405_STABLE master; do
        git push origin refs/remotes/upstream/$BRANCH:refs/heads/$BRANCH
    done
